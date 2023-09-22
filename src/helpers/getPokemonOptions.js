4b1b57b235f00ecdb3cce1583c6ab4031f0688cd
Jhonna <jhonna.somarriba@gmail.com>
2023-09-21 22:07:55 -0600

    First commit

---
+++ src/helpers/getPokemonOptions.js
@@ -0,0 +1,44 @@
+import pokemonApi from "@/api/pokemonApi";
+
+// Creamos el arreglo para obtener los 650 pokemons
+const getPokemons = () => {
+  // Investigar lo de array from
+  const pokemonsArr = Array.from(Array(650));
+  return pokemonsArr.map((_, index) => index + 1);
+}
+
+
+const getPokemonOptions = async() => { 
+
+  // Mezclamos los pokemons
+  const mixedPokemons = getPokemons().sort(() => Math.random() - 0.5);
+  const pokemons = await getPokemonNames(mixedPokemons.splice(0, 4));
+  
+  // console.table(pokemons);
+  return pokemons;
+}
+
+// Funcion para obtener los nombres de los pokemons
+const getPokemonNames =async ([a, b, c, d] = []) => {
+  
+  // Hacemos la peticion a la API
+//  const resp=await pokemonApi.get(`/1`)
+  // Nos ayuda a disparar varias promesas al mismo tiempo
+  const promiseArr = [
+    pokemonApi.get(`/${a}`),
+    pokemonApi.get(`/${b}`),
+    pokemonApi.get(`/${c}`),
+    pokemonApi.get(`/${d}`),
+  ]
+  const [p1,p2,p3,p4] = await Promise.all(promiseArr)
+  
+  return [
+    { name: p1.data.name, id: p1.data.id },
+    { name: p2.data.name, id: p2.data.id },
+    { name: p3.data.name, id: p3.data.id },
+    { name: p4.data.name, id: p4.data.id },
+  ]
+
+ }
+
+export default getPokemonOptions;
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>¿Quien es este Pokemon?</h1>
+
+  <PokemonPicture :pokemonId="5" :showPokemon="true" />
+  <PokemonOptions :pokemons="pokemonArr" />
+</template>
+
+<script>
+import PokemonOptions from "../components/PokemonOptions.vue";
+import PokemonPicture from "../components/PokemonPicture.vue";
+import getPokemonOptions from "../helpers/getPokemonOptions";
+
+// console.log(getPokemonOptions());
+
+export default {
+  name: "PokemonPage",
+  components: {
+    PokemonOptions,
+    PokemonPicture,
+  },
+  data() {
+    return {
+      pokemonArr: [],
+    };
+  },
+  methods: {
+    async mixPokemonsArr() {
+      this.pokemonArr = await getPokemonOptions();
+    },
+  },
+  mounted() {
+    this.mixPokemonsArr();
+  },
+};
+</script>
+
+<style></style>
+<!--  -->
